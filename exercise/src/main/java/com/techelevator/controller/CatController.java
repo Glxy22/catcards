c656514683b4535c797c400b4b606d185f89ffc6
Hewad Noori <hewadnoori16@gmail.com>
2023-03-04 20:40:52 -0500

    finished project

--- exercise/src/main/java/com/techelevator/controller/CatController.java
+++ exercise/src/main/java/com/techelevator/controller/CatController.java
@@ -1,20 +1,68 @@
 package com.techelevator.controller;
 
 import com.techelevator.dao.CatCardDao;
+import com.techelevator.model.CatCard;
+import com.techelevator.model.CatFact;
+import com.techelevator.model.CatPic;
 import com.techelevator.services.CatFactService;
 import com.techelevator.services.CatPicService;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/cards")
 public class CatController {
 
-    private CatCardDao catCardDao;
-    private CatFactService catFactService;
-    private CatPicService catPicService;
+    private CatCardDao cat;
+    private CatFactService catFact;
+    private CatPicService catPic;
 
-    public CatController(CatCardDao catCardDao, CatFactService catFactService, CatPicService catPicService) {
-        this.catCardDao = catCardDao;
-        this.catFactService = catFactService;
-        this.catPicService = catPicService;
+    public CatController(CatCardDao cat, CatFactService catFact, CatPicService catPic) {
+        this.cat = cat;
+        this.catFact = catFact;
+        this.catPic = catPic;
     }
 
+    @RequestMapping(value = "/random", method = RequestMethod.GET)
+    public CatCard makeNewCard(){
+        CatFact f = catFact.getFact();
+        CatPic p = catPic.getPic();
+        CatCard c = new CatCard();
+        c.setCatFact(f.getText());
+        c.setImgUrl(p.getFile());
 
+        return c;
+    }
+
+    @RequestMapping(method = RequestMethod.GET)
+    public List<CatCard> catCardList(){
+        return cat.list();
+    }
+
+    @RequestMapping(path = "/{id}",method = RequestMethod.GET)
+    public CatCard catCardGet(@PathVariable int id){
+        return cat.get(id);
+    }
+
+    @RequestMapping(method = RequestMethod.POST)
+    public CatCard create(@RequestBody CatCard catCard){
+        cat.save(catCard);
+        return catCard;
+    }
+    @RequestMapping(path = "/{id}", method = RequestMethod.PUT)
+    public CatCard update(@RequestBody CatCard catCard, @PathVariable int id){
+        boolean updatedCard = cat.update(id, catCard);
+        if (updatedCard == true){
+            return catCard;
+        }else {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Auction not found");
+        }
+    }
+    @RequestMapping(path = "/{id}", method = RequestMethod.DELETE)
+    public void delete(@PathVariable int id){
+        cat.delete(id);
+    }
 }
--- exercise/src/main/java/com/techelevator/services/RestCatFactService.java
+++ exercise/src/main/java/com/techelevator/services/RestCatFactService.java
@@ -3,14 +3,17 @@ package com.techelevator.services;
 import org.springframework.stereotype.Component;
 
 import com.techelevator.model.CatFact;
+import org.springframework.web.client.RestTemplate;
 
 @Component
 public class RestCatFactService implements CatFactService {
-
+	String API_URL = "https://cat-data.netlify.app/api/facts/random";
+	RestTemplate restTemplate = new RestTemplate();
 	@Override
 	public CatFact getFact() {
 		// TODO Auto-generated method stub
-		return null;
+		CatFact catFact = restTemplate.getForObject(API_URL, CatFact.class);
+		return catFact;
 	}
 
 }
